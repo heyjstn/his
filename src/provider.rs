a454055a333a9e7d74066c0489eb5c4e535cbbc6
Jstn <me@heyjstn.dev>
2026-06-29 19:06:46 +0700

    feat: implement walk dir + some stuffs

--- src/provider.rs
+++ src/provider.rs
@@ -1,8 +1,10 @@
+use crate::RuntimeErr;
+use serde::de::DeserializeOwned;
 use serde::Deserialize;
-use std::any::Any;
 use std::fs;
-use std::fs::{DirEntry, File};
-use std::io::Error;
+use std::fs::DirEntry;
+use std::io;
+use std::str::FromStr;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
@@ -34,12 +36,66 @@ pub struct SessionMessage {
     pub text: String,
 }
 
+pub trait AgentMessage: Sized + DeserializeOwned {
+    fn from_message_str(s: &str) -> Result<Self, RuntimeErr> {
+        serde_json::from_str(s).map_err(|err| RuntimeErr::Generic(err.to_string()))
+    }
+
+    fn read_to_string(path: &str) -> Result<String, RuntimeErr> {
+        fs::read_to_string(path).map_err(|err| RuntimeErr::Generic(err.to_string()))
+    }
+
+    fn parse_one(path: &str) -> Result<Self, RuntimeErr> {
+        let file = Self::read_to_string(path)?;
+        Ok(Self::from_message_str(&file)?)
+    }
+
+    fn parse_vec(path: &str) -> Result<Vec<Self>, RuntimeErr> {
+        let file = Self::read_to_string(path)?;
+        file.lines()
+            .map(|line| Self::from_message_str(line))
+            .collect()
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PiMessage {
+    #[serde(rename = "type")]
+    pub typ: String,
+    pub version: Option<usize>,
+    pub id: String,
+    pub timestamp: String,
+    pub cwd: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CodexMessage {}
+
+impl AgentMessage for PiMessage {}
+
+impl AgentMessage for CodexMessage {}
+
 impl Provider {
     pub fn list_sessions(&self) -> Vec<Session> {
         match self.name {
             ProviderEnum::Pi => {
-                let session_dir = format!("{}/sessions", &self.dir);
-                walk_dir(&session_dir);
+                let dirs = walk_dir(&self.dir).unwrap();
+                let sessions: Vec<Session> = dirs
+                    .iter()
+                    .map(|dir| {
+                        let data: Vec<PiMessage> =
+                            PiMessage::parse_vec(dir.path().as_path().to_str().unwrap()).unwrap();
+                        let initialized_message = data.get(0).unwrap();
+                        Session {
+                            id: initialized_message.id.clone(),
+                            provider: ProviderEnum::Pi,
+                            ts: initialized_message.timestamp.clone(),
+                            cwd: initialized_message.cwd.clone().unwrap(),
+                            messages: None,
+                        }
+                    })
+                    .collect();
+                return sessions;
             }
             ProviderEnum::Codex => {}
         }
@@ -51,10 +107,10 @@ impl Provider {
     }
 }
 
-fn walk_dir(dir: &String) -> Result<Vec<DirEntry>, Error> {
+fn walk_dir(dir: &String) -> Result<Vec<DirEntry>, io::Error> {
     let cur = fs::read_dir(dir)?;
 
-    let list: Vec<Result<DirEntry, Error>> = cur.collect();
+    let list: Vec<Result<DirEntry, io::Error>> = cur.collect();
 
     let mut file_paths: Vec<DirEntry> = vec![];
     for res_file in list {
@@ -74,10 +130,10 @@ fn walk_dir(dir: &String) -> Result<Vec<DirEntry>, Error> {
 #[cfg(test)]
 mod tests {
     use crate::provider::{walk_dir, Provider, ProviderEnum};
-    use std::{env, fs};
+    use std::env;
 
     #[test]
-    fn test_list_sessions() {
+    fn test_list_sessions_1() {
         let cwd = env::current_dir().unwrap().to_str().unwrap().to_string();
         let dir = format!("{}/tests/.codex/sessions", cwd);
 
@@ -90,5 +146,19 @@ mod tests {
 
         let sessions = provider.list_sessions();
         assert!(sessions.is_empty());
+    }
+
+    #[test]
+    fn test_list_sessions_2() {
+        let cwd = env::current_dir().unwrap().to_str().unwrap().to_string();
+        let dir = format!("{}/tests/.pi/agent/sessions", cwd);
+
+        let provider = Provider {
+            name: ProviderEnum::Pi,
+            dir,
+        };
+
+        println!("{:?}", provider.list_sessions());
+        // assert!(!provider.list_sessions().is_empty());
     }
 }
